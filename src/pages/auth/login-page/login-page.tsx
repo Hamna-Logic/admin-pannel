d77ab84cbd8b1c63262dd0a80accbe0230d5b067
Hamna-dev22 <hamna.tdev@gmail.com>
2025-03-01 21:48:29 +0500

    pushedcode

---
+++ src/pages/auth/login-page/login-page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface IProps{
+
+}
+
+const LoginPage:React.FC= ({}:IProps) =>{
+    return(
+        <div>
+            <h1>Login Page</h1>
+        </div>
+    )
+}
+
+export default LoginPage;
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+import LoginPage from "./auth/login-page/login-page";
+import DashboardPage from "./main/dashboard-page/dashboard-page";
+
+export { LoginPage, DashboardPage };
