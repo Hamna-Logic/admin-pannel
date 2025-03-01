c112ef230829f20c9f712f3bb0e42a1421241d23
mudassarfarooq1918 <mudassar@hawklogix.com>
2025-03-01 22:25:11 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { Button, Stack } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Stack direction="horizontal" gap={2}>
